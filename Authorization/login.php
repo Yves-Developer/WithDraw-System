121ac49a3b5abb5302cd9a39531b5d3c928d081f
Yves-Developer <yvesmugisha09@gmail.com>
2023-11-25 04:55:11 -0800

    Organized Files in Folder

--- Authorization/login.php
+++ Authorization/login.php
@@ -53,44 +53,6 @@ E-mail: info@integratedfolks.com
     </section>
 
     <!-- Javascript Linkage -->
-  <script>
-    // Navigation bar
-var logo = document.getElementById('logo');
-var navbar = document.getElementById('navbar');
-var nav = document.getElementById('nav');
-function loadData() {
-    logo.classList.add('active');
-    navbar.classList.add('active');
-    navbar.classList.add('fades');
-    nav.classList.add('active');
-   
-}
-
-var LogBtn = document.getElementsByClassName('btn');
-var form0 = document.querySelector('.col1');
-form0.onsubmit = (Event)=>{
-    Event.preventDefault();
-}
-
-
-LogBtn[0].onclick =()=>{
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '../Assets/Php/Authorise.php', true);
-    xhr.onload = ()=>{
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            var data = xhr.responseText;
-            if (data == "code0") {
-                location.href = "../Admin";
-            }
-            else{
-                alert(data);
-            }
-        }
-    }
-    var formData0 = new FormData(form0);
-    xhr.send(formData0);
-
-}
-  </script>
+<script src="../Assets/Js/login.js"></script>
 </body>
 </html>
